001ec7030b081285faebe4c0a2f4326080e756ff
username <Ryan>
2023-02-24 11:22:19 -0600

    1

--- Components/Banner.tsx
+++ Components/Banner.tsx
@@ -1,4 +1,4 @@
-import Image from "next/legacy/image";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import { baseUrl } from "../Constants/movie";
 import { Movie } from "../typings";
